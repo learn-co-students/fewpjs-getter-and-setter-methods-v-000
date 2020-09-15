d2ec172a9116707ea6bee8e8beffac7ec6065270
benitolsantos <github email address>
2020-09-15 01:37:30 -0400

    Done.

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ class Circle {
 
 
     //Using a getter, you can return a computed value as though it is a property! 
+    //getting is to create psuedo properties
 
     get diameter() {
         return this.radius * 2
@@ -21,7 +22,7 @@ class Circle {
     }
 
     //Using a setter, you can call a function each time the value of a pseudo-property is changed, making sure all data on an class instance is consistent.
-
+    //setting is to create properties
 
     set circumference( circumference ) {
         this.radius = circumference / ( pi * 2 )
