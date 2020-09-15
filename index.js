608fc78c526ce5bb46ec73b6097a62983b86f305
benitolsantos <github email address>
2020-09-15 01:35:57 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Add your Circle class here
+const pi = Math.PI
+class Circle {
+    constructor(radius) {
+        this.radius = radius
+    }
+
+
+    //Using a getter, you can return a computed value as though it is a property! 
+
+    get diameter() {
+        return this.radius * 2
+    }
+
+    get circumference() {
+        return ( this.radius * 2 ) * pi
+    }
+
+    get area() {
+        return pi * ( this.radius * this.radius )
+    }
+
+    //Using a setter, you can call a function each time the value of a pseudo-property is changed, making sure all data on an class instance is consistent.
+
+
+    set circumference( circumference ) {
+        this.radius = circumference / ( pi * 2 )
+    }
+
+    
+    set diameter(diameter) {
+        this.radius = diameter/2
+    }
+
+    set area(area) {
+        this.radius = Math.sqrt( area / pi )
+    }
+}
