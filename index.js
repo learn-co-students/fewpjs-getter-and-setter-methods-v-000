473cd7f26e4acdb183156db19b592d8f1ad05e80
Eric Johnson <ericjohnson0216@icloud.com>
2020-10-07 21:52:55 -0400

    Done.

--- index.js
+++ index.js
@@ -10,11 +10,36 @@
 // Circumference = π • diameter
 // Area = π • radius2
 
-class Circle{
-    constructor(radius){
+
+
+const pi = Math.PI
+
+class Circle {
+    constructor(radius) {
         this.radius = radius;
     }
 
-    
+    get diameter() {
+        return this.radius * 2
+    }
 
+    set diameter(diameter) {
+        this.radius = diameter / 2
+    }
+
+    get circumference() {
+        return (this.radius * 2) * pi
+    }
+
+    set circumference(circumference) {
+        this.radius = circumference / (pi * 2)
+    }
+
+    get area() {
+        return pi * (this.radius * this.radius)
+    }
+
+    set area(area) {
+        this.radius = Math.sqrt(area / pi)
+    }
 }
