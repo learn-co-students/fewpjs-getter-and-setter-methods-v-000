e619e183a7f2f2c502cf0e55005a18c733cfb292
Eric Johnson <ericjohnson0216@icloud.com>
2020-10-07 20:23:56 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Add your Circle class here
+// create a class of Circle
+// Circle will accept 1 parameter, radius, and use this.radius to store the value
+// Use Math.PI to get an accurate measurement of pi(π)
+// Define getter methods for diameter, circumference, and area which will calculate each value using this.radius and pi
+// Define setter methods for diameter, circumference, and area which will accept values for each calculation, calculate the radius based on the input value and set this.radius accordingly
+// For reference, here are the formulas for calculating diameter, circumference and area:
+
+// Diameter = radius • 2
+// Circumference = π • diameter
+// Area = π • radius2
+
+class Circle{
+    constructor(radius){
+        this.radius = radius;
+    }
+
+    
+
+}
