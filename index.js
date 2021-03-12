e6823c68772384d66fc59c999632ad84107f3269
Damien de Lartigue <damien@delartigue.co.uk>
2021-03-12 18:20:39 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 // Add your Circle class here
+
+class Circle {
+  constructor(radius){
+    this.radius = radius;
+  }
+
+  set diameter(diameter) {
+    this.radius = diameter / 2;
+    this._diameter = diameter;
+  }  
+  
+  set circumference(circumference) {
+    this.radius = circumference / (Math.PI * 2);
+    this._circumference = circumference;
+  }  
+  
+  set area(area) {
+    this._area = area;
+  }
+  
+  get diameter() {
+    return this.radius * 2;
+  }
+  
+  get circumference() {
+    return Math.PI * this.diameter;
+  }
+  
+  get area() {
+    return Math.PI * (this.radius * this.radius);
+  }
+}
+
+// class Bird {
+//   constructor(name) {
+//     this.name = name;
+//   }
+ 
+//   set phrase(phrase) {
+//     this._phrase = phrase;
+//   }
+ 
+//   get speak() {
+//     return `${this.name} says ${this._phrase || 'squawk'}`;
+//   }
+// }
